fe458779fd92c9fa5a1c0f9e57df037f7ccca983
Abdunosir <abdunosirusmonov1@gmail.com>
2026-05-21 14:40:07 +0500

    2

--- src/api/admin/urls.py
+++ src/api/admin/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
-from api.admin.views import category_views, author_views, book_views
+from api.admin.views import cotegory_views, auth_views, book_views
 from rest_framework.routers import DefaultRouter
 
 r = DefaultRouter()
 
-r.register("category", category_views.CategoryViewSet, basename="categoryviews")
-r.register("author", author_views.AuthorViewSet, basename="authorviews")
+r.register("category", cotegory_views.CategoryViewSet, basename="categoryviews")
+r.register("author", auth_views.AuthorViewSet, basename="authviews")
 r.register("book", book_views.BookViewSet, basename="bookviews")
 
 
